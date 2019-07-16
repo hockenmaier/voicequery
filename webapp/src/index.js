b511723dd6f3b703e6628b73e4dad1b2991b2aab
Brian Hockenmaier <bhockenmaier@gmail.com>
2019-07-15 21:51:00 -0700

    added draggable to buttons, lifted drag event up to space with id and event available, cleaned up lots of canvas code comments

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -6,7 +6,7 @@ class Bubble extends React.Component{
     render(){  
         return(
             <div>
-                <button className = {this.props.type} height = {200} width = {150}>{this.props.name}</button>
+                <button className = {this.props.type} height = {200} width = {150} draggable="true" onDragStart={this.props.onDrag}>{this.props.name}</button>
             </div>
         );
     }
@@ -16,56 +16,35 @@ class Space extends React.Component{
     constructor(props){
         super(props);
         this.canvasRef = React.createRef();
-        this.state = {          
-          canvas: document.getElementById('space_canvas'),
-          ctx: null,
-        };
-        // console.log(ctx)
       }
 
     componentDidMount(){
-        //creating the canvas here
-        //const canvas = this.refs.canvas
+        //getting the canvas here
         const canvas = this.canvasRef.current;
-        //const mycanvas = React.createContext(canvas)
         const ctx = canvas.getContext('2d');
         this.setState({
             canvas: canvas,
             ctx: ctx
         })
-        
-        // console.log(canvas);
-        // console.log(this.state.canvas);
-        //console.log(this.context.mycanvas)
-        // console.log(ctx);
-        // console.log(this.state.ctx);
-        // console.log(ctx)
         this.resizeCanvas();
         window.addEventListener('resize', this.resizeCanvas.bind(this), false);
     }
     
     resizeCanvas() { //this resizes the canvas and is called when the window size changes
         console.log('resized')
-        // const canvas = this.refs.canvas
         const canvas = this.canvasRef.current;
-        //const canvas = this.state.canvas;
         const SizeX = window.innerWidth*.9;
         const SizeY = window.innerHeight*.6;
         canvas.width = SizeX;
         canvas.height = SizeY;
     }
 
-    // renderBubble(){
-    //     //const canvas = this.refs.canvas
-    //     //const ctx = canvas.getContext('2d');
-    //     console.log("rendering bubble")
-    //     const canvas=this.canvasRef.current
-    //     console.log(canvas)
-
-    //     return <Bubble 
-    //         canvas={this.canvasRef.current}
-    //     />;
-    // }
+    handleDrag(event, id){
+        console.log("draggin in Space");
+        console.log('react SyntheticEvent ==> ', event);
+        console.log('nativeEvent ==> ', event.nativeEvent); //<- gets native JS event
+        console.log('id is: ' + id);
+    }
 
     render(){
         //createCanvas();
@@ -78,33 +57,17 @@ class Space extends React.Component{
                     id= {0}
                     name= {'Costume'}
                     type= {'subject'}
+                    onDrag={(event) => this.handleDrag(event, 0)} 
                 />
                 <Bubble
                     id= {1}
                     name= {'On the Lot'}
                     type= {'condition'}
+                    onDrag={(event) => this.handleDrag(event, 1)} 
                 />
             </div>
         );
     }    
 }
-
-// function createCanvas() {
-//     var canvas = document.getElementById('canvas_picker')
-//     var ctx = canvas.getContext('2d');
-//     var SizeX = 0;
-//     var SizeY = 0;
-//     resizeCanvas();
-//     window.addEventListener('resize', resizeCanvas, false);
-// }
-
-// function resizeCanvas() { //this resizes the canvas and is called when the window size changes
-//     //console.log('resized')
-//     SizeX = window.innerWidth*.95;
-//     SizeY = window.innerHeight*.85;
-//     canvas.width = SizeX;
-//     canvas.height = SizeY;
-//     reDraw();
-// }
 
 ReactDOM.render(<Space />, document.getElementById('root'));
