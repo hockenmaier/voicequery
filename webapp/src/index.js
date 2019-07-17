bad4b496424f463477a9245ab38ecf64dc998776
Brian Hockenmaier <bhockenmaier@gmail.com>
2019-07-16 23:20:58 -0700

    got dragover and drop events working

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Bubble extends React.Component{
-    render(){  
+    allowDrop(){
+        console.log('bubble dragover: ' + this.props.id);
+    }
+
+    render(){
         return(
             <div>
-                <button className = {this.props.type} height = {200} width = {150} draggable="true" onDragStart={this.props.onDrag}>{this.props.name}</button>
+                <button className = {this.props.type} 
+                height = {200} 
+                width = {150} 
+                draggable="true" 
+                onDragStart={this.props.onDrag}
+                onDrop={this.props.onDrop}
+                onDragOver={this.allowDrop.bind(this)}
+                >{this.props.name}
+                </button>
             </div>
         );
     }
 }
+
 
 class Space extends React.Component{
     constructor(props){
@@ -45,25 +58,45 @@ class Space extends React.Component{
         console.log('nativeEvent ==> ', event.nativeEvent); //<- gets native JS event
         console.log('id is: ' + id);
     }
+    handleDrop(event, id){
+        console.log('dropped, id is: ' + id);
+    }
+
+    handleCanvasDrop(event){
+        console.log('canvas drop');
+    }
+
+    handleCanvasDragOver(event){
+        console.log('canvas dragover');
+    }
 
     render(){
         //createCanvas();
         return(
             <div className = "space">                
                 <div className = "canvas">
-                    <canvas ref={this.canvasRef} width={0} height={0} id="space_canvas">Please update your browser to use this app</canvas>
+                    <canvas ref={this.canvasRef} 
+                    width={0} 
+                    height={0} 
+                    id="space_canvas"
+                    onDrop={this.handleCanvasDrop}
+                    onDragOver={this.handleCanvasDragOver}
+                    >Please update your browser to use this app
+                    </canvas>
                 </div>
                 <Bubble
                     id= {0}
                     name= {'Costume'}
                     type= {'subject'}
                     onDrag={(event) => this.handleDrag(event, 0)} 
+                    onDrop={(event) => this.handleDrop(event, 0)} 
                 />
                 <Bubble
                     id= {1}
                     name= {'On the Lot'}
                     type= {'condition'}
-                    onDrag={(event) => this.handleDrag(event, 1)} 
+                    onDrag={(event) => this.handleDrag(event, 1)}
+                    onDrop={(event) => this.handleDrop(event, 1)} 
                 />
             </div>
         );
