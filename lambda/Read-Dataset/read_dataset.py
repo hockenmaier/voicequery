571c010d710b72dc458d6cb7221f28d0ba583c3a
Brian Hockenmaier <bhockenmaier@gmail.com>
2019-09-22 07:40:13 +0000

    used pandas to print sample data and find column names and unique values in columns

--- lambda/Read-Dataset/read_dataset.py
+++ lambda/Read-Dataset/read_dataset.py
@@ -9,27 +9,6 @@ def lambda_handler(event, context):
     return jsonData
     
 def read_dataset():
-    # s3 = boto3.resource(u's3')
-    # bucket = s3.Bucket(u'voicequery-datasets')
-    # obj = bucket.Object(key=u'sample-data/HRData_QuickSightSample.csv')
-    # response = obj.get()
-    
-    # lines = response[u'Body'].read().split()
-    # print(response['Body'].
-    
-    # reader = DictReader(response[u'Body'].read())
-    # print(dir(reader))
-    # print(reader.fieldnames)
-    
-    # with open(response[u'Body'].read()) as csvfile:
-    #     reader = DictReader(csvfile)
-        # print(reader.line_num)
-        # for row in reader:
-        #     print(row)
-    
-    # reader = DictReader(file)
-    # print(reader.fieldnames)
-    
     bucket = "voicequery-datasets"
     file_name = "sample-data/HRData_QuickSightSample.csv"
     
@@ -39,9 +18,26 @@ def read_dataset():
     obj = s3.get_object(Bucket= bucket, Key= file_name) 
     # get object and file (key) from bucket
     
-    initial_df = pd.read_csv(obj['Body']) # 'Body' is a key word
+    hrdata = pd.read_csv(obj['Body']) # 'Body' is a key word
     
-    print(initial_df)
+    print(hrdata[0:5])
+    for i in range(3):
+        print('.')
+    print(hrdata[len(hrdata)-5:len(hrdata)])
+    
+    columns = hrdata.columns
+    #TODO: create bubble objects out of column names
+    #print(hrdata['Tenure'].unique())
+    
+    for col in columns:
+        print('Column: ' + col)
+        print('Unique Values: ')
+        unique = hrdata[col].unique()
+        if (len(unique) < 25):
+            print(unique)
+        else:
+            print('More that 25 distinct values')
+    
     
     jsonData = package_JSON()
     return jsonData
