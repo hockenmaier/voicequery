7e6f4487ff72105926b1a7e08a3d7b8633ba67fc
Brian Hockenmaier <bhockenmaier@gmail.com>
2020-01-20 21:58:34 +0000

    got s3 put working with blob created in python, error with encoding and decoding blob data from webapp

--- lambda/Transcribe/transcribe.py
+++ lambda/Transcribe/transcribe.py
@@ -1,12 +1,13 @@
 import json
 import boto3
+import uuid
 
 def lambda_handler(event, context):
-    jsonData = transcribe(event)
+    jsonData = transcribe(event['blob'])
     return jsonData
 
-def transcribe(event):
-    store_blob_s3(event['blob'])
+def transcribe(blob):
+    store_blob_s3(blob)
     data = {}
     data['statusCode'] = '200'
     data['version'] = "0.0.1"
@@ -15,9 +16,16 @@ def transcribe(event):
 def store_blob_s3(blob):
     bucket = "voicequery-transcribe"
     s3 = boto3.client('s3')
-    file = open('temptranscribe.wav', 'wb')
-    fileBlob = blob.read()
-    file.write(fileBlob)
-    file.close()
-    s3.put_object(Bucket= bucket, Body= file)
-    # obj = s3.get_object(Bucket= bucket, Key= context.file_name)
+    filename = 'temptranscribe_'+ str(uuid.uuid4()) +'.wav'
+    print(blob)
+    # binary_message = blob.encode('utf-8')
+    # fileBlob = blob.read()
+    s3.put_object(Bucket= bucket, Body= blob, Key= filename)
+
+# # -----ENSURE ALL TEST RUNS ARE COMMENTED OUT BEFORE DEPLOYING TO LAMBDA------------------#
+
+# To use this test, the binary created here must be directly passed as the body to the s3 put command
+binary_data = b'\x00\xFF\x00\xFF'
+transcribe(binary_data)
+
+# # -----ENSURE ALL TEST RUNS ARE COMMENTED OUT BEFORE DEPLOYING TO LAMBDA------------------#
