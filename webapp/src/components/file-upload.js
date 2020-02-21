b47558bfd76c0e7b043b7dd614cbc6e9a355202a
Brian Hockenmaier <bhockenmaier@gmail.com>
2020-02-21 06:31:05 +0000

    imported react-dropzone and used default code in file-upload component

--- webapp/src/components/file-upload.js
+++ webapp/src/components/file-upload.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useCallback} from 'react'
 import { Link } from "react-router-dom";
 import './file-upload.css';
+import {useDropzone} from 'react-dropzone'
+
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles)
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+ 
+  return (
+    <div className = 'drop-box'>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          {
+            isDragActive ?
+              <p>Drop the files here ...</p> :
+              <p>Drag 'n' drop some files here, or click to select files</p>
+          }
+        </div>
+    </div>
+  )
+}
 
 class FileUpload extends React.Component {
+    
     render(){
-        return(
+                return(
             <div>
                 <div className = 'file-upload-box'
                     style={{
@@ -14,7 +37,8 @@ class FileUpload extends React.Component {
                             width: window.innerWidth/2,
                             }}
                     >
-                    <h1> upload a new dataset </h1>
+                    <h1>Upload a new dataset</h1>
+                    <MyDropzone />
                 </div>
             </div>
         );
