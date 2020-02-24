8f12cd90b78efd1cd453b2839c89769635a4d7cb
Brian Hockenmaier <bhockenmaier@gmail.com>
2020-02-24 03:26:21 +0000

    added code to save dataset file with presigned URL

---
+++ lambda/Save-Dataset/save-dataset.py
@@ -0,0 +1,63 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+import urllib.request
+import logging
+
+def lambda_handler(event, context):
+    jsonData = transcribe(event['workspace'],event['filename'])
+    return jsonData
+
+def transcribe(workspace,filename):
+    context = create_context(workspace,filename)
+    url = create_presigned_url(context)
+    data = {}
+    data['statusCode'] = '200'
+    data['version'] = "0.0.1"
+    data['presignedurl'] = url
+    data['fileName'] = context.filename
+    print(data)
+    return data
+    
+class contextObject:
+    def __init__(self):
+        # self.blobdata = None
+        self.workspace = ''
+        self.filename = ''
+        self.bucket = ''
+        self.s3 = None
+
+def create_context(workspace,filename):
+    newContext = contextObject()
+    newContext.workspace = workspace
+    newContext.bucket = "voicequery-datasets"
+    newContext.filename = filename
+    newContext.s3 = boto3.client('s3')
+    return newContext
+    
+def create_presigned_url(context, expiration=3600):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+    # Generate a presigned URL for the S3 object
+    try:
+        response = context.s3.generate_presigned_url('put_object',
+                                                    Params={'Bucket': context.bucket,
+                                                            'Key': context.filename},
+                                                            # 'Region': 'us-west-2',
+                                                            # 'ContentType': 'multipart/form-data'},
+                                                            # 'ContentType': 'audio/wav'},
+                                                            # 'ACL': 'public-read',
+                                                            # 'ContentMD5': 'false'},
+                                                    # HttpMethod='Put',
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL
+    return response
